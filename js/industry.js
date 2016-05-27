82df1ac536fbd1a121734f937c86b0a3b1fbe2a5
christine <christine.zxz@gmail.com>
2016-05-27 19:34:58 +0800

    interaction for industry

--- js/industry.js
+++ js/industry.js
@@ -1,7 +1,15 @@
 window.onload = function(){
+    //scrollMagic();
+
     $("#more").bind("click",function(){
-      //console.log("click");
       $(this).toggleClass("rotate1");
+        //if($(this).hasClass("rotate1")){
+        //    $(".scrollmagic-pin-spacer").css("width","75%");
+        //    $("#industryListWrapper").css("width","25%");
+        //}else{
+        //    $("#industryListWrapper").css("width","75%");
+        //    $(".scrollmagic-pin-spacer").css("width","25%");
+        //}
       $(".column_toggle").each(function(){
           $(this).toggle();
       })
@@ -11,7 +19,10 @@ window.onload = function(){
 
     $("#industry_rank_total").bind("click",function(e){
         var index = e.target.parentNode.children[0].innerHTML;
-        console.log(index);
+        var top = parseInt($(e.target.parentNode).offset().top) - parseInt($("#pointer_wrapper").offset().top);
+        //console.log("top:"+top);
+        //console.log(index);
+        $("#pointer").css("top",top+"px");
         changeStockList(index-1);
     })
 };
@@ -19,12 +30,13 @@ window.onload = function(){
 var temp = 0;
 
 function changeStockList(index){
-    $.each($(".select"),function(i,item){
-        $(item).css("opacity","0");
-    });
+    //$.each($(".select"),function(i,item){
+    //    $(item).css("opacity","0");
+    //});
+    //
+    //$($(".select")[index]).css("opacity","1");
+    //console.log($(".select")[index]);
 
-    $($(".select")[index]).css("opacity","1");
-    console.log($(".select")[index]);
     //$("#stock_rank_total").html('<tr>'+
     //    '<th class="column_rank">排名</th>'+
     //    '<th class="column_name_id">股票</th>'+
@@ -55,3 +67,30 @@ function changeStockList(index){
     //}
 
 }
+
+window.onscroll = function(){
+    if(window.scrollY >= 200 ){
+        $("#stockListWrapper").css({
+            "position":"absolute",
+            "top":window.scrollY+100+"px",
+            "right":0
+        });
+    }else{
+        $("#stockListWrapper").css({
+            "position":"relative",
+            "top":0
+        });
+    }
+};
+
+//function scrollMagic(){
+//    var controller = new ScrollMagic.Controller();
+//
+//    var pinBlanket = new ScrollMagic.Scene({
+//        triggerElement: "#industryListWrapper",
+//        triggerHook: "onLeave",
+//        offset: -80
+//        //duration: "100%"
+//    }).addTo(controller)
+//        .setPin("#stockListWrapper");
+//}
